a3d4983fe2e9ba7bb301a49e99557708c002c5e3
spamyolk <kyongsamuel@gmail.com>
2026-01-16 11:37:53 -0800

    Update robot-config.cpp

--- robot-config.cpp
+++ robot-config.cpp
@@ -1,20 +1,16 @@
 #include "vex.h"
 
-
 using namespace vex;
 using signature = vision::signature;
 using code = vision::code;
 
-
 // A global instance of brain used for printing to the V5 Brain screen
 brain  Brain;
-
 
 // VEXcode device constructors
 controller controller_1 = controller(primary);
 
-
-// IMPORTANT: Remember to modify the example motors according to the guide.
+// IMPORTANT: Remember to modify the example motors according to the guide. 
 // Also remember to add respective device declarations to custom/include/robot-config.h
 // Format: motor(port, gearSetting, reversed)
 // gearSetting is one of the following: ratio36_1(red), ratio18_1(green), ratio6_1(blue)
@@ -29,18 +25,6 @@ motor right_chassis2 = motor(PORT18, ratio6_1, false);
 motor right_chassis3 = motor(PORT6, ratio6_1, false);
 motor_group right_chassis = motor_group(right_chassis1, right_chassis2, right_chassis3);
 
-
-/*optical example_optical_sensor = optical(PORT10);
-distance example_distance_sensor = distance(PORT14);
-digital_out example_piston = digital_out(Brain.ThreeWirePort.A);*/
-
-
-// Format is rotation(port, reversed)
-// just set these to random ports if you don't use tracking wheels
-rotation horizontal_tracker = rotation(PORT2, true);
-rotation vertical_tracker = rotation(PORT4, true);
-
-
 // game specific devices for push back
 motor intake_motor1 = motor(PORT5, ratio6_1, false);
 motor intake_motor2 = motor(PORT3, ratio18_1, false);
@@ -53,48 +37,51 @@ digital_out double_park = digital_out(Brain.ThreeWirePort.D);
 digital_out hood = digital_out(Brain.ThreeWirePort.F);
 digital_out wing = digital_out(Brain.ThreeWirePort.H);
 
+// Format is rotation(port, reversed)
+// just set these to random ports if you don't use tracking wheels
+rotation horizontal_tracker = rotation(PORT10, true);
+rotation vertical_tracker = rotation(PORT11, true);
 
-
+// Distance reset sensors
+// Set these to random ports if you are not using distance resets
+distance front_sensor = distance(PORT12);
+distance left_sensor = distance(PORT13);
+distance right_sensor = distance(PORT14);
+distance back_sensor = distance(PORT15);
 
 // game specific devices for high stakes
-/*motor arm_motor1 = motor(PORT2, ratio18_1, true);
-motor arm_motor2 = motor(PORT1, ratio18_1, false);
+motor arm_motor1 = motor(PORT16, ratio18_1, true);
+motor arm_motor2 = motor(PORT17, ratio18_1, false);
 motor_group arm_motor = motor_group(arm_motor1, arm_motor2);
-motor intake_motor = motor(PORT8, ratio18_1, true);
-digital_out claw = digital_out(Brain.ThreeWirePort.F);
-digital_out rush_arm = digital_out(Brain.ThreeWirePort.G);
-optical optical_sensor = optical(PORT18);
+motor intake_motor = motor(PORT18, ratio18_1, true);
+digital_out claw = digital_out(Brain.ThreeWirePort.B);
+digital_out rush_arm = digital_out(Brain.ThreeWirePort.C);
+optical optical_sensor = optical(PORT19);
 distance intake_distance = distance(PORT20);
-distance clamp_distance = distance(PORT12);
-digital_out mogo_mech = digital_out(Brain.ThreeWirePort.E);*/
-
+distance clamp_distance = distance(PORT21);
+digital_out mogo_mech = digital_out(Brain.ThreeWirePort.D);
 
 // ============================================================================
 // USER-CONFIGURABLE PARAMETERS (CHANGE BEFORE USING THIS TEMPLATE)
 // ============================================================================
 
-
 // Distance between the middles of the left and right wheels of the drive (in inches)
-double distance_between_wheels = 11.75;
-
+double distance_between_wheels = 12.3;
 
 // motor to wheel gear ratio * wheel diameter (in inches) * pi
-double wheel_distance_in = (36.0 / 48.0) * 3.25 * M_PI;
-
+double wheel_distance_in = (36.0 / 48.0) * 3.17 * M_PI;
 
 // PID Constants for movement
 // distance_* : Linear PID for straight driving
 // turn_*     : PID for turning in place
 // heading_correction_* : PID for heading correction during linear movement
-double distance_kp = 0.865, distance_ki = 0.0014, distance_kd = 4.664;
-double turn_kp = 0.146, turn_ki = 0.01, turn_kd = 0.6;
+double distance_kp = 1.1, distance_ki = 0.1, distance_kd = 7;
+double turn_kp = 0.3, turn_ki = 0, turn_kd = 2.5;
 double heading_correction_kp = 0.6, heading_correction_ki = 0, heading_correction_kd = 4;
-
 
 // Enable or disable the use of tracking wheels
 bool using_horizontal_tracker = false;  // Set to true if a horizontal tracking wheel is installed and used for odometry
 bool using_vertical_tracker = false;   // Set to true if a vertical tracking wheel is installed and used for odometry
-
 
 // IGNORE THESE IF YOU ARE NOT USING TRACKING WHEELS
 // These comments are in the perspective of a top down view of the robot when the robot is facing vertical
@@ -105,22 +92,31 @@ double vertical_tracker_dist_from_center = -0.03125;
 double horizontal_tracker_diameter = 1.975; // Diameter of the horizontal tracker wheel (in inches)
 double vertical_tracker_diameter = 1.975; // Diameter of the vertical tracker wheel (in inches)
 
+// Distance Reset setup
+// Set all of these values to the distance from the respective distance sensor to the robot's center along the axis it faces(in inches)
+// The front sensor offset is the distance from the front distance sensor to the robot center along the Y axis
+// The back sensor offset is the distance from the back distance sensor to the robot center along the Y axis
+// The left sensor offset is the distance from the left distance sensor to the robot center along the X axis
+// The right sensor offset is the distance from the right distance sensor to the robot center along the X axis
+// All values should be positive numbers
+// If you are not using all four distance sensors, just set the unused ones to 0
+// If you are not using distance resets these values will be ignored
+double front_sensor_offset = 0.0;
+double left_sensor_offset = 0.0;
+double right_sensor_offset = 0.0;
+double back_sensor_offset = 0.0;
 
 // ============================================================================
 // ADVANCED TUNING (OPTIONAL)
 // ============================================================================
 
-
 bool heading_correction = true; // Use heading correction when the bot is stationary
-
 
 // Set to true for more accuracy and smoothness, false for more speed
 bool dir_change_start = true;   // Less accel/decel due to expecting direction change at start of movement
 bool dir_change_end = true;     // Less accel/decel due to expecting direction change at end of movement
 
-
 double min_output = 10; // Minimum output voltage to motors while chaining movements
-
 
 // Maximum allowed change in voltage output per 10 msec during movement
 double max_slew_accel_fwd = 24;
@@ -128,28 +124,24 @@ double max_slew_decel_fwd = 24;
 double max_slew_accel_rev = 24;
 double max_slew_decel_rev = 24;
 
-
 // Prevents too much slipping during boomerang movements
 // Decrease if there is too much drifting and inconsistency during boomerang
 // Increase for more speed during boomerang
 double chase_power = 2;
 
-
 // ============================================================================
 // DO NOT CHANGE ANYTHING BELOW
 // ============================================================================
-
 
 // VEXcode generated functions
 // define variable for remote controller enable/disable
 bool RemoteControlCodeEnabled = true;
 
-
 /**
-* Used to initialize code/tasks/devices added using tools in VEXcode Pro.
-*
-* This should be called at the start of your int main function.
-*/
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
 void vexcodeInit(void) {
- // nothing to initialize
+  // nothing to initialize
 }
