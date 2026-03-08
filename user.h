f4f0fedd0fd1ddce8d70a67bacfd2ca687f717ab
spamyolk <kyongsamuel@gmail.com>
2026-03-08 15:53:24 -0700

    Update user.h

--- user.h
+++ user.h
@@ -1,9 +1,16 @@
 void runAutonomous();
 void runDriver();
 void runPreAutonomous();
+
 void PID_test();
-void Leftside();
-void Rightside();
+void Test();
+void Seven_R();
+void redGoalRush();
 void SAWP();
+void LeftS();
+void LeftF();
+void RightS();
+void RightF();
+void RightF2();
 void Skills();
-void Elims();
+void SAWP();
