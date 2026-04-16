4738b67a7f64b24746ad88360af3038f3fa51939
spamyolk <kyongsamuel@gmail.com>
2026-04-16 13:35:30 -0700

    Update autonomous.cpp

--- autonomous.cpp
+++ autonomous.cpp
@@ -168,26 +168,28 @@ void Left7() {
   
   //collect 3B, middle-low
   intake(-12, 12, 12);
-  moveToPoint(-11, 22.5, 1, 1500, false);
+  moveToPoint(-11.25, 18.5, 1, 1500, false); //11, 22.5
   fd.set(true);
-  moveToPoint(-13, 25.5, 1, 750, false);
+  moveToPoint(-13.25, 21.5, 1, 750, false); //13, 25.5
+
 
   //collect 3B, loader-r2
-  moveToPoint(-48, 4, 1, 1000, false);
+  moveToPoint(-30, 3, 1, 1000, false); //48, 4
   turnToAngle(-180, 500);
-  driveTo(16.25, 825, true, 6.5);
+  driveTo(16.25, 825, true, 6.5); //16.25
   wait(175, msec);
 
   //score 7B, long-r2
-  driveTo(-30, 1000, true);
+  driveTo(-29.5, 1000, true);
   hood.set(true);
   fd.set(false);
   wait(1000, msec);
 
   //wing, long-r2 EDIT THIS
-  moveToPoint(-31, 8, 1, 1000);
+  moveToPoint(-20, 14, 1, 1000);
   wing.set(false);
-  moveToPoint(-39, 32, 1, 2000, true, 6.0);
+  turnToAngle(0, 1000);
+  driveTo(30, 1000, true, 8.0);
   intake(0, 0, 0);
   turnToAngle(45, 1000);
   stopChassis(hold);
@@ -330,148 +332,23 @@ void Right43() {
 void Skills() {
   //calibrate
   correct_angle = inertial_sensor.rotation(); //correct angle variable to inertial sensor
+  //wing.set(true);
+  
+  //move
+  //calibrate
+  correct_angle = inertial_sensor.rotation(); //correct angle variable to inertial sensor
   wing.set(true);
-
-  //collect 6B, loader-r1
-  moveToPoint(-19.65, 0, 1, 2000, false);
-  fd.set(true);
-  turnToAngle(-180, 1000);
   
-  //move, middle-high -> collect+score 2B, middle-high
+  //collect 3B, middle-low
   intake(-12, 12, 12);
-  moveToPoint(-10, 12, 1, 1500, false);
-  turnToAngle(-135, 1000);
-  intake(-12, -4, -4);
-  driveTo(-14.5, 1500);
-  intake(-12, 7, -5);
-  wait(600, msec);
-
-  //move, loader-r1 -> collect 6B
-  moveToPoint(-12.5, -5, 1, 2000, false);
+  moveToPoint(11, 22.5, 1, 1500, false, 8);
   fd.set(true);
-  turnToAngle(-180, 1000);
-  intake(-12, 12, 12);
-  driveTo(11.5, 1000, true, 6.0);
-  wait(1400, msec);
+  moveToPoint(13, 25.5, 1, 750, false, 8);
+  turnToAngle(-45, 1000);
+  driveTo(12, 1000);
+  intake(6, -12, 0);
 
-  //move, loader-b2
-  driveTo(-8, 1000);
-  intake(0, 0, 0);
-  turnToAngle(180, 800);
-  fd.set(false);
-  moveToPoint(-25, 14, -1, 3000, false);
-  turnToAngle(180, 800);
-
-  x_pos = 0;
-  y_pos = 0;
-  moveToPoint(0, 50, -1, 4000);
-
-  turnToAngle(90, 800);
-  driveTo(8.4, 2000);
-  turnToAngle(0, 800);
-  driveTo(-14, 1500, true, 8.0);
-
-  //score 6B, long-b2
-  hood.set(true);
-  wait(100, msec);
-  intake(-12, 12, 12);
-  wait(2200, msec);
-  
-  //collect 6B, loader-b2
-  fd.set(true);
-  driveTo(14, 1000);
-  hood.set(false);
-  intake(-12, 12, 12);
-  turnToAngle(0, 1000);
-  driveTo(12, 700, true, 6.0);
-  wait(1400, msec);
-
-  //score 6B, loader-b1
-  intake(0, 0, 0);
-  driveTo(-14, 1500);
-  turnToAngle(0, 1000);
-  driveTo(-8, 750, true, 8.5);
-  hood.set(true);
-  wait(100, msec);
-  intake(-12, 12, 12);
-  wait(2200, msec);
-  fd.set(false);
-  intake(0, 0, 0);
-  x_pos = 0;
-  y_pos = 0;
-
-
-  //move, loader-b1
-  driveTo(8, 1000);
-  hood.set(false);
-  turnToAngle(90, 1000);
-  driveTo(62.6, 4000, true, 10.0);
-  turnToAngle(0, 1000);
-  driveTo(-13, 1000);
-  wait(250, msec);
-
-
-  //collect 6B, loader-b1
-  fd.set(true);
-  driveTo(14, 1000);
-  hood.set(false);
-  intake(-12, 12, 12);
-  turnToAngle(0, 1000);
-  driveTo(12, 700, true, 6.0);
-  wait(1400, msec);
-
-  //move, loader-r2
-  driveTo(-8, 1000);
-  intake(0, 0, 0);
-  turnToAngle(0, 800);
-  fd.set(false);
-  turnToAngle(-45, 800);
-  driveTo(-12.5, 1000);
-  turnToAngle(0, 800);
-
-  x_pos = 0;
-  y_pos = 0;
-  moveToPoint(0, -50, -1, 4000);
-
-  turnToAngle(-90, 800);
-  driveTo(7.75, 2000);
-  turnToAngle(180, 800);
-  driveTo(-14, 1500, true, 9.0);
-
-  //score 6B, long-b2
-  hood.set(true);
-  wait(100, msec);
-  intake(-12, 12, 12);
-  wait(2200, msec);
-  
-  //collect 6B, loader-b2
-  fd.set(true);
-  driveTo(14, 1000);
-  hood.set(false);
-  intake(-12, 12, 12);
-  turnToAngle(180, 800);
-  driveTo(12, 700, true, 6.0);
-  wait(1400, msec);
-
-  //score 6B, loader-b1
-  intake(0, 0, 0);
-  driveTo(-14, 1500);
-  turnToAngle(180, 800);
-  driveTo(-8, 750, true, 8.25);
-  hood.set(true);
-  wait(100, msec);
-  intake(-12, 12, 12);
-  wait(2200, msec);
-  fd.set(false);
-  intake(0, 0, 0);
-
-  //move, RP
-  wing.set(false);
-  curveCircle(-90, 36, 1500);
-  hood.set(false);
-  intake(12, 12, -12);
-  driveTo(-2, 1000);
-  driveTo(24, 2000, true, 10.0);
+  moveToPoint(42, 0, -1, 1000, 8);
 }
 
 
